7e74e8af01a3c5a824c70fcac3d7eaf1a4249698
gitynity <nitybriliantluckybro@gmail.com>
2018-10-09 16:59:57 +0530

    Anagram
    T(n) = O(n)
    Space = O(n)

---
+++ anagram.cpp
@@ -0,0 +1,62 @@
+/*
+ * given a word , say , 'abc' --> its anagram is 'acb , bac , bca , cab , cba'
+ * i.e any word conatining only these letters in any order.
+ * 
+ * Note : The word may contain special charcters and numbers also.
+ * The word will not contain a tab or a space.
+ * 
+ * My Solution
+ * Time Complexity = O(n);
+ * Space Complexity = O(n);
+ * 
+*/ 
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	int a[127] {0};
+	
+	string s = "cbaa";
+	
+	for(size_t i =0 ;i<s.size();i++)
+	{
+		if(s[i])
+			a[(int) s[i]]++;
+	}
+	
+//	for(int i =0 ;i<=127;i++)
+//	{
+//		cout<<(char)i<<"="<<a[i]<<endl;
+//	}
+//	
+
+	string tv = "abac";
+	int b[127] {0};
+	for(size_t i =0 ;i<tv.size();i++)
+	{
+		if(tv[i])
+			b[(int) tv[i]]++;
+	}
+	
+
+	bool flag = true;
+
+	for(size_t i =0;i<=127;i++)
+	{
+		if(a[i]==b[i])
+			continue;
+		else
+		{
+			flag = false;
+			break;
+		}
+	}
+	
+	if(flag)
+		cout<<"Anagram\n";
+		else cout<<"Not anagram\n";
+	
+	return 0;
+}
