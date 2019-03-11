0fae0858a783f1f2392e0be01d0d97c6a068bed1
gitynity <nitybriliantluckybro@gmail.com>
2019-03-11 19:54:33 +0530

    Create manipulatorsInCpp2.cpp
    second in series of C++ manipulators

---
+++ manipulatorsInCpp2.cpp
@@ -0,0 +1,22 @@
+//setfill('*') fills the remaining empty fields in setw() statements by *.
+//setfill('*') will remain active until end of the program.
+
+#include <iomanip>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout<<setfill('*');
+	cout<<setw(5)<<1<<endl;
+	cout<<setw(5)<<10<<endl;
+	cout<<setw(5)<<100<<endl;
+	cout<<setw(5)<<"---"<<endl;
+	cout<<setw(5)<<111<<endl;
+	return 0;
+}
+
+
+//iomanip.h is header file which contains manipulator operators and manipulator functions.
+//ex: manipulator operators----> endl , ends.
+//ex: manipulator functions----> setw() , setprecision() , setfill() , setiosflags().
