f5ea4d0193bb76ab8727aa2cea951b8ca7aa8cb5
gitynity <nitybriliantluckybro@gmail.com>
2020-04-12 00:42:20 +0530

    Update how to input sentences in c++.cpp

--- how to input sentences in c++.cpp
+++ how to input sentences in c++.cpp
@@ -15,20 +15,27 @@ int main()
 	cout<<greeting<<endl;
 	cout<<left_over<<endl;
 	
-	//When i enter "hello there" into the greeting,
-	//after "hello" ,cin encounters a space and assigns the input 'before the space' to greeting
-	//but the 'input after the space' is still remaining in the cin
-	//so the next variable (here left_over) gets it 
+	//When I try to enter "hello there" into the greeting variable , I am doing that using cin object.
+	//After "hello" ,cin encounters a SPACE and assigns the input 'before the SPACE' to the 'greeting' variable
+	//but the 'input after the SPACE' is still remaining in the cin buffer
+	//so the next variable (here 'left_over') gets the 'input after the SPACE' until either another SPACE is encountered  
+	//or the input is finished.
 	
-	//to make this clear , lets input a sentence in one variable through cin
+	//To make this more clear , lets input a sentence in one variable through cin
 	
 	string namaste;
 	cin>>namaste;
 	cout<<namaste<<endl;
+	
 	//lets input "india is big"
 	//when we cout namaste , we just get "india" 
+	//"is big" is still in the cin buffer
+	//if we had two more variables taking input through the cin object ,one would get "is" and other would get "big" 
 	
-	cin.ignore(numeric_limits<streamsize>::max(), '\n');		//to clear the cin buffer	
+	
+	//Now lets clear the cin buffer
+	cin.ignore(numeric_limits<streamsize>::max(), '\n');	//remember that cin is an object and it has many member functions	
+								//ignore() is one of those functions							
 	
 	
 	//now lets use getline() to input the complete sentence
