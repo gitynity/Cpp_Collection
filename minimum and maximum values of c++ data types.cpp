a811370fc80a74ee51443bb004f3eef2677e007e
gitynity <nitybriliantluckybro@gmail.com>
2020-04-11 19:16:10 +0530

    Create minimum and maximum values of c++ data types.cpp

---
+++ minimum and maximum values of c++ data types.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include<climits>
+using namespace std;
+
+int main() {
+	
+	cout<<"Maximum value for a short is: "<<SHRT_MAX<<endl;
+	cout<<"Maximum value for an unsigned short is: "<<USHRT_MAX<<endl;
+	cout<<"Minimum value for a short is: "<<SHRT_MIN<<endl;
+	cout<<"Minimum value for an unsigned short is: "<<0<<endl<<endl;
+	
+	
+	cout<<"Maximum value for an int is: "<<INT_MAX<<endl;
+	cout<<"Maximum value for an unsigned int is: "<<UINT_MAX<<endl;
+	cout<<"Minimum value for an int is: "<<INT_MIN<<endl;
+	cout<<"Minimum value for an unsigned int is: "<<0<<endl<<endl;
+	
+	
+	cout<<"Maximum value for a long is: "<<LONG_MAX<<endl;
+	cout<<"Maximum value for an unsigned long is: "<<ULONG_MAX<<endl;
+	cout<<"Minimum value for a long is: "<<LONG_MIN<<endl;
+	cout<<"Minimum value for an unsigned long is: "<<0<<endl<<endl;
+	
+	
+	cout<<"Maximum value for a long long is: "<<LLONG_MAX<<endl;
+	cout<<"Maximum value for an unsigned long long is: "<<ULLONG_MAX<<endl;
+	cout<<"Minimum value for a long long is: "<<LLONG_MIN<<endl;
+	cout<<"Minimum value for an unsigned long long is: "<<0<<endl;
+	
+	return 0;
+}
+
+/*
+
+Maximum value for a short is: 32767
+Maximum value for an unsigned short is: 65535
+Minimum value for a short is: -32768
+Minimum value for an unsigned short is: 0
+
+Maximum value for an int is: 2147483647
+Maximum value for an unsigned int is: 4294967295
+Minimum value for an int is: -2147483648
+Minimum value for an unsigned int is: 0
+
+Maximum value for a long is: 9223372036854775807
+Maximum value for an unsigned long is: 18446744073709551615
+Minimum value for a long is: -9223372036854775808
+Minimum value for an unsigned long is: 0
+
+Maximum value for a long long is: 9223372036854775807
+Maximum value for an unsigned long long is: 18446744073709551615
+Minimum value for a long long is: -9223372036854775808
+Minimum value for an unsigned long long is: 0
+
+*/
