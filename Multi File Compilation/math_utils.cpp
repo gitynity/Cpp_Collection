309b18f6039ea681d3e6f07b80369782550f3396
gitynity <nitybriliantluckybro@gmail.com>
2020-04-18 23:35:18 +0530

    Create math_utils.cpp

---
+++ Multi File Compilation/math_utils.cpp
@@ -0,0 +1,25 @@
+#include "math_utils.h"
+
+double pow(double base , int pow)
+{
+	int total = 1;
+	for(int i=0;i<pow;i++)
+		total = base*total;
+
+	return total;
+}
+
+double area(double length , double width)
+{
+	return length*width;
+}
+
+double area(double length)
+{
+	return pow(length);
+}
+
+double area(Rectangle r)
+{
+	return r.length*r.width;
+}
