a32f8a8721beacc78f4080569d7d5d6beaa5d922
gitynity <nitybriliantluckybro@gmail.com>
2020-04-20 04:02:54 +0530

    All declarations are here.
    What is in this file?
    *Including header files 
    *Using namespaces 
    *Function declarations

---
+++ Multi File Compilation 2/declarations.h
@@ -0,0 +1,19 @@
+#ifndef DECLARATIONS
+#define DECLARATIONS
+
+#include<iostream>
+#include<string>		
+#include<fstream>
+
+using namespace std;
+
+//You can include all the header files and namespaces that you need in your project here. 
+//So that you won't have to include them in any other files.
+//Just make sure to include this file in all other files.
+
+
+
+void the_ultimate_truth(string file);
+
+
+#endif
