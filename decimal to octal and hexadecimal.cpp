b5731b6b1560d87929c11c81144c416389c19376
Nityanand Sharma <nityanand.official@gmail.com>
2020-11-04 10:24:23 +0530

    Create decimal to octal and hexadecimal.cpp

---
+++ decimal to octal and hexadecimal.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+int n;
+cin>>n;
+cout<<"Octal representation of "<<n<<": "<<oct<<n;
+cout<<"\n";
+cout<<"Hexadecimal representation of "<<n<<": "<<hex<<n;
+return 0;
+}
